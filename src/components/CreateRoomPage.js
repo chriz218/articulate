a715cfc09fd5cc9866b37a641afaded89f3339f8
Christopher Thong <christhong123@gmail.com>
2020-03-11 17:30:26 +0800

    Created the first design for the create room page

--- src/components/CreateRoomPage.js
+++ src/components/CreateRoomPage.js
@@ -1,12 +1,42 @@
 import axios from 'axios'
 import React, { useState, useEffect } from 'react'
-import { Route, Link } from "react-router-dom"
+import { Route, Link, useHistory } from "react-router-dom"
+import '../CSSFiles/CreateRoomPage.css';
 
 function CreateRoomPage() {
 
+    let history = useHistory();
+
+    const handleCancel = () => {
+        history.push("/")
+    }
+
     return (
         <div>
-            Create Room
+            <h1>Articulate</h1>
+            <form action="#" method="POST">
+                <div className="form-content">
+                    <label>Room Code:</label>
+                    <input type="text" name="Room Code" placeholder="Will be randomly generated..." />
+                    <label>Your name:</label>
+                    <input type="text" name="Your name" placeholder="Enter your name here..." />
+                    <label>No. of Teams:</label>
+                    <select name="No. of Teams">
+                        <option value="1">1</option>
+                        <option value="2">2</option>
+                        <option value="3">3</option>
+                        <option value="4">4</option>
+                    </select>
+                    <label>List of Players:</label>
+                    <div className="players-list">
+                        List of players will be here
+                    </div>
+                </div>
+                <div className="BtnDiv">
+                    <button type="submit" id="PlayBtn">Play!</button>
+                    <button id="CancelBtn" onClick={handleCancel}>Cancel</button>
+                </div>
+            </form>
         </div>
     );
 
