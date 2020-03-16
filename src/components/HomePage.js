c91944ae556fbd861f09d02c55ed6a323bc0c46d
Christopher Thong <christhong123@gmail.com>
2020-03-16 18:35:08 +0800

    Changed the homepage styling, so that it appears quite decent in different screen sizes

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -2,7 +2,7 @@ import axios from 'axios'
 import React, { useState, useEffect } from 'react'
 import { Route, Link, useHistory } from "react-router-dom"
 import '../CSSFiles/HomePage.css';
-import {GENERATE_DEVICE} from '../properties';
+import { GENERATE_DEVICE } from '../properties';
 
 // NEED TO FIX THE HEIGHT
 
@@ -11,11 +11,11 @@ function HomePage({ socket, setIsHost, socketId, setSocketId }) {
 
     useEffect(() => {
         socket.emit('getSocketId', {}, (error) => {
-            if(error) {
+            if (error) {
                 alert(error);
             }
         });
-    },[]);
+    }, []);
 
     useEffect(() => {
         socket.on("socketId", ({ socketId }, error) => {
@@ -25,14 +25,14 @@ function HomePage({ socket, setIsHost, socketId, setSocketId }) {
     });
 
     const handleCreateRoom = () => {
-        if(socketId) {
+        if (socketId) {
             setIsHost(true);
             history.push("/create");
         }
     };
 
     const handleJoinRoom = () => {
-        if(socketId) {
+        if (socketId) {
             setIsHost(false);
             history.push("/join");
         }
@@ -40,28 +40,28 @@ function HomePage({ socket, setIsHost, socketId, setSocketId }) {
 
     return (
         <div>
-            <h1>Articulate</h1>
-                <h2 id="RuleTitle">Rules</h2>
-                    <div id="Description">
-                        Describe the generated word(s) for your teammates to guess. You cannot spell or rhyme the word, nor can you say the word or parts of the word. Each teammate will take turns being the describer and guesser. Each word guessed correctly moves your board piece by 1 position. Move your board piece to the end position to win. The color of the box where your board piece is on determines the word category.
-                    </div>
-                    <div className="Squares" style={{backgroundColor: 'cyan'}}/>
-                    <div className="ColorDescription">Object (e.g., tool box, pool table)</div>
-                    <div className="Squares" style={{backgroundColor: '#FF7B00'}}/>
-                    <div className="ColorDescription">Action (e.g., singing, driving)</div>
-                    <div className="Squares" style={{backgroundColor: 'green'}}/>
-                    <div className="ColorDescription">Nature (e.g., tulip, rose)</div>
-                    <div className="Squares" style={{backgroundColor: 'blue'}}/>
-                    <div className="ColorDescription">World (e.g., The Louvre, Senegal)</div>
-                    <div className="Squares" style={{backgroundColor: 'yellow'}}/>
-                    <div className="ColorDescription">Person (e.g., Peter Griffin, Cain)</div>
-                    <div className="Squares" style={{backgroundColor: 'red'}}/>
-                    <div className="ColorDescription">Random (e.g., chapter, wedge)</div>
-                    <div className="Squares" style={{backgroundColor: 'grey'}}/>
-                    <div className="ColorDescription">Everything (All categories)</div>
+            <h1 id="ArticulateTitle">Articulate</h1>
+            <h2 id="RuleTitle">Rules</h2>
+            <div id="Description">
+                Describe the generated word(s) for your teammates to guess. You cannot spell or rhyme the word, nor can you say the word or parts of the word. Each teammate will take turns being the describer and guesser. Each word guessed correctly moves your board piece by 1 position. Move your board piece to the end position to win. The color of the box where your board piece is on determines the word category.
+            </div>
+            <div className="Squares" style={{ backgroundColor: 'cyan' }} />
+            <div className="ColorDescription">Object (e.g., tool box, pool table)</div>
+            <div className="Squares" style={{ backgroundColor: '#FF7B00' }} />
+            <div className="ColorDescription">Action (e.g., singing, driving)</div>
+            <div className="Squares" style={{ backgroundColor: 'green' }} />
+            <div className="ColorDescription">Nature (e.g., tulip, rose)</div>
+            <div className="Squares" style={{ backgroundColor: 'blue' }} />
+            <div className="ColorDescription">World (e.g., The Louvre, Senegal)</div>
+            <div className="Squares" style={{ backgroundColor: 'yellow' }} />
+            <div className="ColorDescription">Person (e.g., Peter Griffin, Cain)</div>
+            <div className="Squares" style={{ backgroundColor: 'red' }} />
+            <div className="ColorDescription">Random (e.g., chapter, wedge)</div>
+            <div className="Squares" style={{ backgroundColor: 'grey' }} />
+            <div className="ColorDescription">Everything (All categories)</div>
             <div id="HomeBtnsDiv">
                 <button className="HomeBtns" onClick={handleCreateRoom}>Create Room</button>
-                <button className="HomeBtns" onClick={handleJoinRoom}>Join Room</button> 
+                <button className="HomeBtns" onClick={handleJoinRoom}>Join Room</button>
             </div>
         </div>
     );
