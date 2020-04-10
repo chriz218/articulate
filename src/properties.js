52a97005523b4c4575d06a39968330965eadb82c
Zheng Hung Chuah <zheng_hung@hotmail.com>
2020-04-10 17:11:45 +0100

    fixup! backend url

--- src/properties.js
+++ src/properties.js
@@ -1,5 +1,5 @@
-// export const BACKEND_ENDPOINT = 'https://articulate-be.herokuapp.com/';
-export const BACKEND_ENDPOINT = 'http://localhost:5000/';
+export const BACKEND_ENDPOINT = 'https://articulate-be.herokuapp.com/';
+// export const BACKEND_ENDPOINT = 'http://localhost:5000/';
 
 export const CREATE_ROOM = BACKEND_ENDPOINT + 'roomcreate';
 
